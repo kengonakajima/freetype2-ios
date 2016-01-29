bd1aca27724213ed51cacf50b9fbebf20c0efc18
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-01-30 07:35:47 +0900

    ios 9.2

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-xcodebuild -configuration Release -sdk iphoneos8.1 -alltargets clean
-xcodebuild -configuration Release -sdk iphoneos8.1 -alltargets build
+xcodebuild -configuration Release -sdk iphoneos9.2 -alltargets clean
+xcodebuild -configuration Release -sdk iphoneos9.2 -alltargets build
 cp build/Release-iphoneos/libFreetype2.a ./libFreetype2-arm.a
 
-xcodebuild -configuration Release -sdk iphonesimulator8.1 -alltargets clean
-xcodebuild -configuration Release -sdk iphonesimulator8.1 -alltargets build
+xcodebuild -configuration Release -sdk iphonesimulator9.2 -alltargets clean
+xcodebuild -configuration Release -sdk iphonesimulator9.2 -alltargets build
 cp build/Release-iphonesimulator/libFreetype2.a ./libFreetype2-sim.a
 
 lipo -output libFreetype2.a -create libFreetype2-arm.a -arch i386 libFreetype2-sim.a
